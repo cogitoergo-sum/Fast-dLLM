020a73062b51ee419ea68bb093d89ef19dec4f8a
Gary Huang <ioi@gl-login4.arc-ts.umich.edu>
2025-10-19 16:34:09 -0400

    merged run script

--- eval_sample.sh
+++ eval_sample.sh
@@ -1,9 +1,29 @@
-module load cuda/12.8.1
-module load cudnn/12.8-v9.10.0
+#!/bin/bash
+
+#SBATCH --job-name=fast-dlm-run
+#SBATCH --account=cse585f25_class
+#SBATCH --partition=spgpu
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=60g
+#SBATCH --gpus=a40:1
+
+module load cuda
+module load cudnn
 module load uv
+
+uv run python -c "import torch; exit(0 if torch.cuda.is_available() else 1)"
+
+if [ $? -eq 0 ]; then
+    echo "CUDA available for PyTorch"
+else
+    echo "Failed to use CUDA for PyTorch, exiting..."
+    exit
+fi
+
 export HF_ALLOW_CODE_EVAL=1
 export HF_DATASETS_TRUST_REMOTE_CODE=true
-
 
 task=gsm8k
 length=256
@@ -12,7 +32,7 @@ num_fewshot=5
 steps=$((length / block_length))
 factor=1.0
 model_path='GSAI-ML/LLaDA-8B-Instruct'
-limit=32
+limit=128
 
 # Baseline - full quality generation with 256 steps
 uv run accelerate launch llada/eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
@@ -27,3 +47,4 @@ uv run accelerate launch llada/eval_llada.py --tasks ${task} --num_fewshot ${num
 --output_path ./results_gsm8k_parallel \
 --confirm_run_unsafe_code --model llada_dist \
 --model_args model_path=${model_path},gen_length=${length},steps=${steps},block_length=${block_length},threshold=0.9,save_dir=./test_gsm8k_parallel,show_speed=True
+
