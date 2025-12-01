854a9988188d5002741c77f07c9cca328d305f24
Kai <kaijun@umich.edu>
2025-12-01 01:49:50 -0500

    fix test script

--- llada/test_eval_llada.sh
+++ llada/test_eval_llada.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#SBATCH --job-name=fast-dlm-run-base
+#SBATCH --account=cse585f25_class
+#SBATCH --partition=spgpu
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=60g
+#SBATCH --gpus=a40:1
+
+module load cuda
+module load cudnn
+module load uv
+
+uv run python -c "import torch; exit(0 if torch.cuda.is_available() else 1)"
+
+if [ $? -eq 0 ]; then
+    echo "CUDA available for PyTorch"
+else
+    echo "Failed to use CUDA for PyTorch, exiting..."
+    exit
+fi
+
 # Set the environment variables first before running the command.
 export HF_ALLOW_CODE_EVAL=1
 export HF_DATASETS_TRUST_REMOTE_CODE=true
@@ -14,8 +36,8 @@ model_path='GSAI-ML/LLaDA-8B-Instruct'
 # Run a quick test with limit=2
 echo "Running quick test for eval_llada.py..."
 load_results_path='results/samples_gsm8k_2025-11-19T23-07-40.279237.jsonl'
-# accelerate launch eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
-accelerate launch --cpu eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
+uv run accelerate launch eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
+# accelerate launch --cpu eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
 --confirm_run_unsafe_code --model llada_dist \
 --model_args model_path=${model_path},gen_length=${length},steps=${length},block_length=${block_length},show_speed=True,load_results_path=${load_results_path} \
 --limit 2
