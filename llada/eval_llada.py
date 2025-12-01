25812d8ec238f7a021e079f2043c7c0530e0defd
Kai <kaijun@umich.edu>
2025-12-01 01:39:46 -0500

    fix import

--- llada/eval_llada.py
+++ llada/eval_llada.py
@@ -34,7 +34,7 @@ from lm_eval.api.registry import register_model
 from tqdm import tqdm
 import os
 from transformers import AutoTokenizer, AutoModel, AutoConfig
-from generate import generate, generate_with_prefix_cache, generate_with_dual_cache
+from generate import generate, generate_with_prefix_cache, generate_with_dual_cache, wrapper_generate
 from model.modeling_llada import LLaDAModelLM
 import json
 import time
